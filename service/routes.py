85f40a5fe7e8c162904f3b2ea987b4b78b5954cc
Muh021 <shaikh.muhammadali987@gmail.com>
2025-06-28 15:43:54 -0400

    Implement Delete account endpoint + tests

--- service/routes.py
+++ service/routes.py
@@ -112,8 +112,18 @@ def update_account(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
-
+@app.route(f"{BASE_URL}/<int:account_id>", methods=["DELETE"])
+def delete_account(account_id):
+    """
+    Delete an Account by ID
+    """
+    app.logger.info("Request to delete Account %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND,
+              f"Account with id [{account_id}] could not be found.")
+    account.delete()
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
