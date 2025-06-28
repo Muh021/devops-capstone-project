a0d4288580f65ac803499e6763eeef1f693ffc5f
Muh021 <shaikh.muhammadali987@gmail.com>
2025-06-28 16:34:06 -0400

    Fix blank-line errors to satisfy flake8 E302

--- service/routes.py
+++ service/routes.py
@@ -4,12 +4,13 @@ Account Service
 This microservice handles the lifecycle of Accounts
 """
 # pylint: disable=unused-import
-from flask import jsonify, request, make_response, abort, url_for   # noqa; F401
+from flask import jsonify, request, make_response, abort, url_for  # noqa; F401
 from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
 
 BASE_URL = "/accounts"
+
 
 ############################################################
 # Health Endpoint
@@ -51,17 +52,17 @@ def create_accounts():
     account.deserialize(request.get_json())
     account.create()
     message = account.serialize()
-    # Uncomment once get_accounts has been implemented
-    # location_url = url_for("get_accounts", account_id=account.id, _external=True)
-    location_url = "/"  # Remove once get_accounts has been implemented
+    # Uncomment once get_account has been implemented
+    # location_url = url_for("get_account", account_id=account.id, _external=True)
+    location_url = "/"  # Remove once get_account has been implemented
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-
 @app.route(BASE_URL, methods=["GET"])
 def list_accounts():
     """
@@ -84,15 +85,16 @@ def get_account(account_id):
     app.logger.info("Request to read Account %s", account_id)
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND,
-              f"Account with id [{account_id}] could not be found.")
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id [{account_id}] could not be found.",
+        )
     return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
-
 @app.route(f"{BASE_URL}/<int:account_id>", methods=["PUT"])
 def update_account(account_id):
     """
@@ -101,8 +103,10 @@ def update_account(account_id):
     app.logger.info("Request to update Account %s", account_id)
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND,
-              f"Account with id [{account_id}] could not be found.")
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id [{account_id}] could not be found.",
+        )
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
@@ -111,7 +115,6 @@ def update_account(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
 @app.route(f"{BASE_URL}/<int:account_id>", methods=["DELETE"])
 def delete_account(account_id):
     """
@@ -120,16 +123,17 @@ def delete_account(account_id):
     app.logger.info("Request to delete Account %s", account_id)
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND,
-              f"Account with id [{account_id}] could not be found.")
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id [{account_id}] could not be found.",
+        )
     account.delete()
     return "", status.HTTP_204_NO_CONTENT
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
